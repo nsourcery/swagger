f60a7d5c610251a132cfc695bc0ecb5a682f1ec3
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-09-28 08:03:25 -0300

    fix: using env as a di

--- src/auto-swagger/auto-swagger.service.spec.ts
+++ src/auto-swagger/auto-swagger.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MockEnvironmentService } from '@nsourcery/common';
+import { EnvironmentService } from '@nsourcery/env';
 import { AutoSwaggerService } from './auto-swagger.service';
 
 describe('SourceryService', () => {
@@ -6,7 +8,10 @@ describe('SourceryService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AutoSwaggerService],
+      providers: [
+        AutoSwaggerService,
+        { provide: EnvironmentService, useClass: MockEnvironmentService },
+      ],
     }).compile();
 
     service = module.get<AutoSwaggerService>(AutoSwaggerService);
--- src/auto-swagger/auto-swagger.service.ts
+++ src/auto-swagger/auto-swagger.service.ts
@@ -4,22 +4,23 @@ import { EnvironmentService } from '@nsourcery/env';
 
 @Injectable()
 export class AutoSwaggerService {
-  addSwagger(
-    app: INestApplication,
-    env: EnvironmentService,
-    prefix: string = 'APP',
-  ): void {
+  constructor(private readonly env: EnvironmentService) {}
+  addSwagger(app: INestApplication, prefix: string = 'APP'): void {
     const options = new DocumentBuilder()
-      .setTitle(env.get(`${prefix}_TITLE`) || 'Sample API')
+      .setTitle(this.env.get(`${prefix}_TITLE`) || 'Sample API')
       .setDescription(
-        env.get(
+        this.env.get(
           `${prefix}_DESCRIPTION` ||
             'This is a sample API, use it as you like.',
         ),
       )
-      .setVersion(env.get(`${prefix}_VERSION`) || 'v1')
+      .setVersion(this.env.get(`${prefix}_VERSION`) || 'v1')
       .build();
     const docs = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup(env.get(`${prefix}_DOCS` || 'api-docs'), app, docs);
+    SwaggerModule.setup(
+      this.env.get(`${prefix}_DOCS` || 'api-docs'),
+      app,
+      docs,
+    );
   }
 }
